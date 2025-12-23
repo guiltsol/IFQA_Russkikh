f300cfa8378f74c3a8f4d96b7340918d30ee74f3
MaximRusskikh <maksimrusskikh@yandex.ru>
2025-12-23 12:15:06 +0300

    Исправил кодировку ридера

--- src/main/java/hw_3/utils/CustomProperties.java
+++ src/main/java/hw_3/utils/CustomProperties.java
@@ -3,6 +3,8 @@ package hw_3.utils;
 import lombok.Getter;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
@@ -13,8 +15,10 @@ public class CustomProperties {
     private static final Properties props = new Properties();
 
     public static void loadProperties() {
-        try {
-            props.load(Files.newInputStream(Paths.get("src/test/resources/config.properties")));
+        try (InputStreamReader reader = new InputStreamReader(
+                Files.newInputStream(Paths.get("src/test/resources/config.properties")),
+                StandardCharsets.UTF_8)) {
+            props.load(reader);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
